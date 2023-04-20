de85262be21fc407205a7532e804d2b2da6484bc
Wilmer Antonio Mosquera Figueroa <114489499+wworkinhome@users.noreply.github.com>
2023-04-20 09:07:51 -0500

    Creación del proyecto

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'my-custom-color': '#fffff'
+      },
+      screens: {
+        xs: '375px',
+        sm: '600px',
+        md: '900px',
+        lg: '1200px',
+        xl: '1536px',
+      },
+    },
+  },
+  plugins: [],
+}
